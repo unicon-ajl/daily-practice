cf45c422f53b1f754eccec881b226c7badd748a3
ajl <ajl@gmail.com>
2022-12-14 16:09:58 +0800

    second commit

---
+++ day01/src/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="child">
+    <h1>我是Child组件（子组件）</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  components: {
+  },
+  setup() {
+  }
+};
+</script>
+
+<style>
+.child{
+  background: orange;
+  padding: 10px;
+}
+</style>
---
+++ day01/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+// console.log('vue3的应用对象',app)
+app.mount('#app')
